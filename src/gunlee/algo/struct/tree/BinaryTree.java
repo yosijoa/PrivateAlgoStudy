a863597bb011fa2fe3695f06bbda3d9cbb0c71e8
yosijoa <yosijoa@gmail.com>
2015-01-23 15:59:06 +0900

    modify binary search for int

--- src/gunlee/algo/struct/tree/BinaryTree.java
+++ src/gunlee/algo/struct/tree/BinaryTree.java
@@ -44,15 +44,15 @@ public class BinaryTree {
 	}
 	
 	public static void main(String[] args) {
-		Node a = new Node("a");
-		Node b = new Node("b");
-		Node c = new Node("c");
-		Node d = new Node("d");
-		Node e = new Node("e");
-		Node f = new Node("f");
-		Node g = new Node("g");
-		Node h = new Node("h");
-		Node i = new Node("i");
+		Node a = new Node(1);
+		Node b = new Node(2);
+		Node c = new Node(3);
+		Node d = new Node(4);
+		Node e = new Node(5);
+		Node f = new Node(6);
+		Node g = new Node(7);
+		Node h = new Node(8);
+		Node i = new Node(9);
 		
 		a.setLeft(b);
 		a.setRight(c);
